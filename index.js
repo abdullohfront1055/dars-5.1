30f9b3fda20efd16eac593076f943bafc3336747
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-13 11:41:21 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let sonda = Number(prompt("Birorta hafta kunini kiriting"));
+
+switch (sonda) {
+    case 1: alert("Dushanba"); break;
+    case 2: alert("Seshanba"); break;
+    case 3: alert("Chorshaba"); break;
+    case 4: alert("Payshnab"); break;
+    case 5: alert("Juma"); break;
+    case 6: alert("Shanba"); break;
+    case 7: alert("Yakshnaba"); break;
+}
